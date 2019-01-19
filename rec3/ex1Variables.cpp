f2dc6085b3637c018013604d17e3ecfa3ce54d5a
Tetsumichi Umada <tetsumichi.umada@gmail.com>
2019-01-19 14:41:50 -0700

    feat: Add ex for variables

---
+++ rec3/ex1Variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main(int argc, char const *argv[])
+{
+	int myInt = 0; 
+	double myDouble = 3.1415926535; 
+	string myStr = "Geoffrey Hinton"; 
+
+	cout << myStr << endl; 
+
+	return 0;
+}
