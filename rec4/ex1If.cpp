53494d038f275061f0183777a2f90d0de4be3736
Tetsumichi Umada <tetsumichi.umada@gmail.com>
2019-01-19 14:49:08 -0700

    feat: Add if example

---
+++ rec4/ex1If.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+
+int main(int argc, char const *argv[])
+{
+	int num = 0; 
+
+	if(num > 20){
+		cout <<  num << " is greater than 20" << endl; 
+	} else {
+		cout << "well yeah" << endl; 
+	}
+
+	return 0;
+}
+
